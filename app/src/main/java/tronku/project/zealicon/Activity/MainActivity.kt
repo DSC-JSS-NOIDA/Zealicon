e23be9005188404e28691d092bbd95706d4fb590
tronku <shubham.pathak2000@gmail.com>
2020-02-07 21:59:42 +0530

    Setting up project

---
+++ app/src/main/java/tronku/project/zealicon/Activity/MainActivity.kt
@@ -0,0 +1,13 @@
+package tronku.project.zealicon.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tronku.project.zealicon.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
