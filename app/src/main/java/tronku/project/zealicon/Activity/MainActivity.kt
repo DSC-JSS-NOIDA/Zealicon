b3ad1fff87a8f95c7a6fe10ab895badfc8edc028
dheerajkotwani <dheeraj.kotwani41@gmail.com>
2020-02-10 19:09:52 +0530

    update nav drawer

--- app/src/main/java/tronku/project/zealicon/Activity/MainActivity.kt
+++ app/src/main/java/tronku/project/zealicon/Activity/MainActivity.kt
@@ -1,27 +1,46 @@
 package tronku.project.zealicon.Activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.opengl.Visibility
+import android.os.Build
 import android.os.Bundle
-import android.view.View
+import android.widget.Toolbar
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuAdapter
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
 import androidx.navigation.findNavController
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.RecyclerView
-import com.bvapp.arcmenulibrary.ArcMenu
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.android.synthetic.main.activity_main.*
+import nl.psdcompany.duonavigationdrawer.views.DuoDrawerLayout
+import nl.psdcompany.duonavigationdrawer.views.DuoMenuView
+import nl.psdcompany.duonavigationdrawer.widgets.DuoDrawerToggle
 import tronku.project.zealicon.R
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var navController: NavController
+    lateinit var duoDrawerLayout: DuoDrawerLayout
+    lateinit var duoMenuView: DuoMenuView
+    lateinit var menuAdapter: MenuAdapter
+    lateinit var menuOptions: ArrayList<String>
+    lateinit var mToolbar: androidx.appcompat.widget.Toolbar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initializeViews()
         setupNavigation()
+        navDrawerIcon.setOnClickListener {
+            duoDrawerLayout.openDrawer()
+        }
+    }
+
+
+    private fun initializeViews() {
+        duoDrawerLayout = findViewById(R.id.duoDrawerLayout)
+        duoMenuView = findViewById(R.id.duoMenuView)
+        mToolbar = findViewById(R.id.toolbar)
     }
 
     private fun setupNavigation() {
@@ -31,46 +50,4 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSupportNavigateUp() = navController.navigateUp()
 
-    val ITEM_DRAWABLES = listOf( R.color.red_100,
-        R.color.green_100,
-        R.color.red_100,
-        R.color.green_100,
-        R.color.red_100,
-        R.color.green_100)
-
-    val STR = listOf("Home",
-        "Route",
-        "Connect",
-        "Sponsor",
-        "Team",
-        "About")
-
-  /*  var arcMenu: ArcMenu =  findViewById(R.id.arcMenu)
-    arcMenu.showTooltip=true
-    menu.setToolTipBackColor(Color.WHITE);
-    menu.setToolTipCorner(6f);
-    menu.setToolTipPadding(2f);
-    menu.setToolTipTextColor(Color.BLUE);
-    menu.setAnim(300,300,ArcMenu.ANIM_MIDDLE_TO_RIGHT,ArcMenu.ANIM_MIDDLE_TO_RIGHT,
-    ArcMenu.ANIM_INTERPOLATOR_ACCELERATE_DECLERATE,ArcMenu.ANIM_INTERPOLATOR_ACCELERATE_DECLERATE);
-
-    final int itemCount = ITEM_DRAWABLES.length;
-    for (int i = 0; i < itemCount; i++) {
-        FloatingActionButton item = new FloatingActionButton(this);
-
-        item.setSize(FloatingActionButton.SIZE_MINI); // set initial size for child, it will create fab first
-        item.setIcon(itemDrawables[i]); // It will set fab icon from your resources which related to 'ITEM_DRAWABLES'
-        item.setBackgroundColor(getResources().getColor(R.color.colorPrimary)); // it will set fab child's color
-        menu.setChildSize(item.getIntrinsicHeight()); // set absolout child size for menu
-
-        val position: Int = i;
-        menu.addItem(item, str[i], new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //You can access child click in here
-            }
-        });
-    }
-
-   */
 }
--- app/src/main/java/tronku/project/zealicon/Fragment/SearchFragment.kt
+++ app/src/main/java/tronku/project/zealicon/Fragment/SearchFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.search_fragment.*
 
 import tronku.project.zealicon.R
+import tronku.project.zealicon.Utils.AnimUtils
 import tronku.project.zealicon.Viewmodel.SearchViewModel
 
 class SearchFragment : Fragment() {
@@ -29,6 +31,13 @@ class SearchFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(SearchViewModel::class.java)
         // TODO: Use the ViewModel
+
+        AnimUtils.setTouchEffect(cat_one)
+        AnimUtils.setTouchEffect(cat_two)
+        AnimUtils.setTouchEffect(cat_three)
+        AnimUtils.setTouchEffect(cat_four)
+        AnimUtils.setTouchEffect(cat_five)
+        AnimUtils.setTouchEffect(cat_six)
     }
 
 }
